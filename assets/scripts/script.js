5c175884d89928d3e8aa3368489c091029d03429
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2024-04-17 12:20:47 +0530

    data is added and links are working now

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,36 +5,13 @@ const mainContainer = document.querySelector('.items-container');
 
 let cartDataIdArray = [];
 
-addMainScreenItem(items);  //add html content to main screen from items data array.
-
-function addMainScreenItem(dataArray) {
-    const itemsContainer = document.querySelector(".items-container");
-    let innerHtmlData = ``;
-    dataArray.forEach((dataObject) => {
-        innerHtmlData += `<div class="item-container">
-        <img class="item-image" src="${dataObject.image}" alt="ItemImage" />
-        <div class="rating">${dataObject.rating.stars}⭐ | ${dataObject.rating.count}</div>
-        <div class="brand-name">${dataObject.company}</div>
-        <div class="item-name">${dataObject.item_name}</div>
-        <div class="price">
-        <span class="current-price">Rs ${dataObject.current_price}</span>
-        <span class="original-price">Rs ${dataObject.original_price}</span>
-        <span class="discount">(${dataObject.discount_percentage}% OFF)</span>
-        </div>
-        <button class="cart-button" onclick="addToCart('${dataObject.id}')">Add to Bag</button>
-        </div>`
-
-    })
-    itemsContainer.innerHTML = innerHtmlData;
-}
-
 function addItemOfId(id) {
     const cartItemDetails = document.querySelector(".cart-item-details");
     const dataFromId = items.find((data) => data.id == id);
     let cartInnerHtmlData =
         `<div class="cart-item-template" id="a${id}">
         <div class="cart-item-image">
-        <img src="${dataFromId.image}" alt="ItemImage" />
+        <img src="${dataFromId.image.image1}" alt="ItemImage" />
         </div>
         <div class="template-content">
             <div class="cart-brand-name">${dataFromId.company}</div>
